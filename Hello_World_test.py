93d861b3c63d6c75724b3111ccf19bd79bffcbd1
JZTynes <joshuatynes@hotmail.com>
2021-01-25 11:02:38 -0400

    First commit

---
+++ Hello_World_test.py
@@ -0,0 +1,17 @@
+""" /**************************************************
+Advance Embedded Systems Class
+Intro to methods
+Student: Joshua Z. Tynes
+Student #: W0415592
+Instructor: Ricardo Bautista Quintero
+Date:January 19th 2021
+NSCC Ivany Campus
+/**************************************************"""
+
+def method():
+    print("Hello World\n")
+    a=(1,2,3,4,5)
+    print(type(a))
+    print(a)
+if __name__ == '__main__':
+    method()
